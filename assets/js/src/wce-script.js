da3f5bf99abe35bdef8763b4ffb5a9b52ad4ffb5
Sidsector9 <siddharth.thevaril@gmail.com>
2017-08-15 01:59:07 +0530

    Added working thumb icons

--- assets/js/src/wce-script.js
+++ assets/js/src/wce-script.js
@@ -1,7 +1,7 @@
 ( function( $ ) {
 
 	var self = {
-		vote_button: '',
+		vote_button : '',
 	};
 
 	self.init = function() {
@@ -11,8 +11,10 @@
 
 	self.vote_click = function() {
 		self.vote_button.on( 'click', function() {
-			var comment_id = $( this ).data( 'comment-id' );
-			var vote_type  = $( this ).data( 'vote-type' );
+			var that       = $( this );
+			var wce        = $( '.wce-vote-button' );
+			var comment_id = that.data( 'comment-id' );
+			var vote_type  = that.data( 'vote-type' );
 
 			$.ajax({
 				url : wce_ajax_url,
@@ -24,7 +26,47 @@
 				},
 			})
 			.done( function( response ) {
+				if ( $.isArray( response.data ) && 'vote switched' === response.data[0] ) {
+					if ( 'up' === response.data[1] ) {
+						that
+							.siblings( '.wce-vote-button' )
+							.find( 'i' )
+							.removeClass( 'fa-thumbs-down' )
+							.addClass( 'fa-thumbs-o-down' );
 
+						that
+							.find( 'i' )
+							.removeClass( 'fa-thumbs-o-up' )
+							.addClass( 'fa-thumbs-up' );
+					}
+
+					if ( 'down' === response.data[1] ) {
+						that
+							.siblings( '.wce-vote-button' )
+							.find( 'i' )
+							.removeClass( 'fa-thumbs-up' )
+							.addClass( 'fa-thumbs-o-up' );
+
+						that
+							.find( 'i' )
+							.removeClass( 'fa-thumbs-o-down' )
+							.addClass( 'fa-thumbs-down' );
+					}
+				} else {
+					if ( 'up' === vote_type ) {
+						that
+							.find( 'i' )
+							.removeClass( 'fa-thumbs-o-up' )
+							.addClass( 'fa-thumbs-up' );
+					}
+
+					if ( 'down' === vote_type ) {
+						that
+							.find( 'i' )
+							.removeClass( 'fa-thumbs-o-down' )
+							.addClass( 'fa-thumbs-down' );
+					}
+				}
 			});
 		});
 	};
--- wp-comments-extras.php
+++ wp-comments-extras.php
@@ -5,22 +5,79 @@
 
 class WP_Comments_extras {
 
+	private $user_id = null;
+
 	public function __construct() {
+		wp_enqueue_style( 'wce-style', plugins_url( '/assets/css/wce-style.css', __FILE__ ), null, null, null );
+		wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 		wp_enqueue_script( 'wce-script', plugins_url( '/assets/js/src/wce-script.js', __FILE__ ), array( 'jquery' ), null, true );
 		wp_localize_script( 'wce-script', 'wce_ajax_url', admin_url( 'admin-ajax.php' ) );
 		add_action( 'wp_ajax_save_votes', array( $this, 'save_votes' ) );
 		add_action( 'wp_ajax_nopriv_save_votes', array( $this, 'save_votes' ) );
+		add_action( 'init', array( $this, 'get_user_id' ) );
 		add_filter( 'comment_reply_link_args', array( $this, 'add_voting_buttons' ), 10, 2 );
 	}
 
+	public function get_user_id() {
+		$this->user_id = get_current_user_id();
+	}
+
 	public function add_voting_buttons( $args, $comment ) {
-		$args['after'] = '<span class="wce-vote-button" data-comment-id="' . $comment->comment_ID . '" data-vote-type="up">UP</span>';
-		$args['after'] .= '<span class="wce-vote-button" data-comment-id="' . $comment->comment_ID . ' data-vote-type="down">DOWN</span></div>';
+		$comment_id = $comment->comment_ID;
+		$users      = get_comment_meta( $comment_id, 'votes', true );
+		$user_id    = $this->user_id;
+
+		if ( 'up' === $users[ $user_id ] ) {
+			$voted     = 'wce-voted-up';
+			$vote_icon = '<i class="fa fa-thumbs-up" aria-hidden="true"></i>';
+		} else {
+			$vote = null;
+			$vote_icon = '<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>';
+		}
+		$args['after'] = '<span class="wce-vote-button ' . $voted . '" data-comment-id="' . $comment_id . '" data-vote-type="up">' . $vote_icon . '</span>';
+
+		$voted = null;
+		if ( 'down' === $users[ $user_id ] ) {
+			$voted     = 'wce-voted-down';
+			$vote_icon = '<i class="fa fa-thumbs-down" aria-hidden="true"></i>';
+		} else {
+			$vote = null;
+			$vote_icon = '<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>';
+		}
+		$args['after'] .= '<span class="wce-vote-button ' . $voted . '" data-comment-id="' . $comment_id . '" data-vote-type="down">' . $vote_icon . '</span></div>';
+
 		return $args;
 	}
 
 	public function save_votes() {
-		
+		$comment_id = absint( filter_input( INPUT_POST, 'comment_id', FILTER_SANITIZE_STRING ) );
+		$vote_type  = filter_input( INPUT_POST, 'vote_type', FILTER_SANITIZE_STRING );
+		$user_id    = $this->user_id;
+		$users      = get_comment_meta( $comment_id, 'votes', true );
+		$vote_data  = array();
+		$vote_other = 'up' === $vote_type ? 'up' : 'down';
+
+		if ( empty( $users ) ) {
+			// The first vote.
+			$vote_data = array( $user_id => $vote_type );
+			add_comment_meta( $comment_id, 'votes', $vote_data, true )
+			? wp_send_json_success()
+			: wp_send_json_error();
+		} elseif ( isset( $users[ $user_id ] ) && $users[ $user_id ] === $vote_type ) {
+			// Voting twice of the same type.
+			wp_send_json_error( $data );
+		} elseif ( isset( $users[ $user_id ] ) ) {
+			// Switching votes.
+			$users[ $user_id ] = $vote_other;
+			update_comment_meta( $comment_id, 'votes', $users )
+			? wp_send_json_success( array( 'vote switched', $vote_type ) )
+			: wp_send_json_error( array( 'vote switch failed' ) );
+		} else {
+			// Add more votes.
+			$users[ $user_id ] = $vote_type;
+			update_comment_meta( $comment_id, 'votes', $users );
+		}
+
 		wp_die();
 	}
 }
